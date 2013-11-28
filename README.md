f25f7c3abfb31ebcfda555804f5997a56887d2b5
@nelsonic <nodecoder@gmail.com>
2013-11-28 14:35:57 +0000

    set up project with background info and basic code from question

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ node-parse-rss
 ==============
 
 A quick node.js RSS feed parser.
+
+Original **quest**ion:
+http://stackoverflow.com/questions/20177259/requesting-rss-feeds-from-two-web-sites-in-node-js
+Saw this question on Stack Overflow and decided it merited a quick answer.
+
+The author also posted it on LinkedIn:
+http://www.linkedin.com/groups/Parse-RSS-feeds-using-Nodejs-2906459.S.5811745652475990020
+
+## Discussion
+
+From the original code on StackOverflow Maksim is only using one *non-core* 
+module: **feed-read**
+
+See: 
+- https://npmjs.org/package/feed-read
+- https://github.com/sentientwaffle/feed-read
+
+I don't understand the choice of 3rd Party Module.
+feed-read has only **5 watchers** at the time of writing (*not v. popular*)
+but it does have **unit tests** which is a good sign so lets try it!
---
+++ app.js
@@ -0,0 +1,51 @@
+var feed = require('feed-read');
+var http = require('http');
+var async = require('async');
+var request = require('request');
+
+var LIMIT = 10;
+var UNABLE_TO_CONNECT = "Unable to connect.";
+var BBC_URL = 'http://feeds.bbci.co.uk/news/rss.xml';
+var SKY_URL = 'http://news.sky.com/feeds/rss/home.xml';
+
+var server = http.createServer(onRequest);
+server.listen(9000);
+
+function onRequest(req, res) {
+    res.writeHead(200, {
+        'Content-Type' : 'text/html; charset=utf-8'
+    });
+
+    async.parallel([ function(callback) {
+        feed(BBC_URL, onRssFetched);
+        // TODO: where to call callback()?
+    }, function(callback) {
+        feed(SKY_URL, onRssFetched);
+        // TODO: where to call callback()?
+    } ], function done(err, results) {
+        console.log("Done");
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+function onRssFetched(err, articles) {
+    console.log("RSS fetched");
+    var html = [];
+    if (err) {
+        html.push("<p>", UNABLE_TO_CONNECT = "</p>");
+    } else {
+        html.push("<ol>");
+        var i = 0;
+        articles.forEach(function(entry) {
+            if (i == LIMIT) {
+                return;
+            }
+            html.push("<li><a href='" + entry.link + "'>" + entry.title
+                    + "</a></li>");
+            i++;
+        });
+    }
+    console.log(html.join(""));
+}
